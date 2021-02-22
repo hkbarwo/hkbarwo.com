f0e70ba00ab338586c6a623a00d755aa89680578
Ng Wing Tat, David <i@ngwingt.at>
2021-02-23 01:43:14 +0800

    ✨ Add landing page full screen slide

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -22,6 +22,83 @@ function useSlideSize(slider) {
   };
 }
 
+function FullScreenSlide({ slide, i, total, onClose, onNext, onPrev }) {
+  return (
+    <li
+      key={slide.slug}
+      className="absolute inset-0"
+    >
+      <div
+        className="relative flex h-full"
+        href={`#${slide.slug}`}
+        style={{
+          backgroundImage:
+            `linear-gradient(to bottom, ${slide.gradient.color1} 0% , ${slide.gradient.color2} 100%)`,
+        }}
+      >
+        <div className="hidden lg:block relative flex-1 overflow-hidden">
+          <img className="w-full h-full object-cover" src={`${slide.bgImage}`} alt={slide.title} />
+        </div>
+        <div className="flex-1 text-white">
+          <div className="p-10 sm:p-32 md:p-64 w-full">
+            <div className="flex items-center justify-between">
+              <div className="hidden lg:block mr-24 text-18 tracking-wide flex-grow">{slide.shortTitle}</div>
+              <div className="mr-24 flex items-center">
+                <button
+                  className="w-20 h-20"
+                  onClick={onPrev}
+                >
+                  <svg className="w-20 h-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M18.45,10.13H1.76m8.59-8.8L1.68,10l8.68,8.67" fill="none" stroke="currentColor" stroke-width="1"/>
+                  </svg>
+                </button>
+                <span className="mx-8 text-22 font-light tracking-widest leading-none">{`${i < 9 ? '0' : ''}${i + 1}/${total < 9 ? '0' : ''}${total + 1}`}</span>
+                <button
+                  className="w-20 h-20"
+                  onClick={onNext}
+                >
+                  <svg className="w-20 h-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path d="M1.68,9.88H18.37M9.78,18.67,18.45,10,9.77,1.33" fill="none" stroke="currentColor" stroke-width="1"/>
+                  </svg>
+                </button>
+              </div>
+              <button className="relative block w-16 h-16 md:w-28 md:h-28" onClick={onClose}>
+                <svg className="absolute inset-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28">
+                  <path d="M27,1,1,27M1,1,27,27" fill="none" stroke="currentColor"/>
+                </svg>
+              </button>
+            </div>
+            <div className="flex flex-col md:mr-48">
+              <div className="md:hidden mt-32 text-18 tracking-wide text-center">{slide.shortTitle}</div>
+              <h1 className="mt-16 md:mt-40 text-36 font-bold font-serif tracking-widest text-center md:text-left">{slide.title}</h1>
+              <h2 className="mt-4 text-28 font-light font-serif tracking-widest text-center md:text-left">{slide.subtitle}</h2>
+              <p className="mt-44 text-14 leading-8 whitespace-pre-wrap">{slide.description}</p>
+              <a
+                className="mt-60 mx-auto flex items-center py-10 px-24 rounded-full border border-white bg-white bg-opacity-25 group"
+                href="#"
+              >
+                {slide.buttonTitle}
+                <svg
+                  className="ml-12 w-12 h-12 transform group-hover:translate-x-4 transition-transform"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 12 12"
+                >
+                  <path
+                    d="M1.39 6h9.3M5.86 1.16L10.69 6l-4.84 4.83"
+                    fill="none"
+                    stroke="#fff"
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
+
 export default function HomePageTemplate({ pageContext }) {
   const slideCount = pageContext.slides.length;
   const maxSlideIndex = slideCount - 1;
@@ -32,6 +109,8 @@ export default function HomePageTemplate({ pageContext }) {
 
   const [preciseSlideIndex, setPreciseSlideIndex] = useState(0);
   const slideIndex = Math.round(preciseSlideIndex);
+
+  const [isShowDetail, setIsShowDetail] = useState(false);
 
   const clampedSlideIndex = useMemo(() => {
     return Math.min(preciseSlideIndex, maxSlideIndex - slidesPerPage + 1);
@@ -74,12 +153,24 @@ export default function HomePageTemplate({ pageContext }) {
     window.scrollTo(0, scrollY);
   }, [preciseSlideIndex, maxSlideIndex, slidesPerPage]);
 
+  const goToSlide = useCallback((index) => {
+    const scrollHeight = document.body.scrollHeight - window.innerHeight;
+    const scrollY = scrollHeight / maxSlideIndex * index;
+    window.scrollTo(0, scrollY);
+  }, [maxSlideIndex])
+
   function nextPage() {
     goToPage(1);
   }
 
   function prevPage() {
     goToPage(-1);
+  }
+
+  function onClickSlide(e, index) {
+    e.preventDefault();
+    goToSlide(index);
+    setIsShowDetail(true);
   }
 
   return (
@@ -114,6 +205,7 @@ export default function HomePageTemplate({ pageContext }) {
                 <a
                   className={classNames('flex h-full', i % 2 === 0 ? 'flex-col-reverse' : 'flex-col')}
                   href={`#${slide.slug}`}
+                  onClick={(e) => onClickSlide(e, i)}
                 >
                   <div className="relative flex-grow overflow-hidden">   
                     <img
@@ -204,7 +296,7 @@ export default function HomePageTemplate({ pageContext }) {
             key={slide.slug}
             className="text-white"
           >
-            <a href={`#${slide.slug}`}>
+            <a href={`#${slide.slug}`} onClick={(e) => onClickSlide(e, i)}>
               <div className="aspect-w-1 aspect-h-1">   
                 <img
                   className="object-cover"
@@ -241,6 +333,35 @@ export default function HomePageTemplate({ pageContext }) {
           </li>
         ))}
       </ul>
+      {isShowDetail && (
+        <section className="z-50 md:z-0 fixed inset-0">
+          <ul>
+            <FullScreenSlide
+              slide={pageContext.slides[slideIndex]}
+              i={slideIndex}
+              total={maxSlideIndex}
+              onClose={() => setIsShowDetail(false)}
+              onNext={() => goToSlide(slideIndex + 1)}
+              onPrev={() => goToSlide(slideIndex - 1)}
+            />
+          </ul>
+          <div className="hidden absolute right-0 inset-y-0 w-60 md:flex items-center justify-center">
+            <ul>
+              {pageContext.slides.map((slide, i) => (
+                <li
+                  key={slide.slug}
+                  className={classNames(
+                    'w-10 h-10 my-8 border border-white rounded-full transition-colors',
+                    {
+                      'bg-white': i === slideIndex,
+                    },
+                  )}
+                />
+              ))}
+            </ul>
+          </div>
+        </section>
+      )}
       <PageFooter className="md:hidden" {...{ pageContext }} />
     </main>
   )
