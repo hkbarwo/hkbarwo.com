d62ae3b6dd9b7a4976d4f7f4884d8414f6c9eba9
Ng Wing Tat, David <i@ngwingt.at>
2021-03-01 17:22:14 +0800

    🐛 Fix news details page logo link

--- src/templates/NewsDetailsPage.js
+++ src/templates/NewsDetailsPage.js
@@ -17,6 +17,7 @@ export default function NewsDetailsPage({ path, pageContext }) {
           pageTitle={<FormattedMessage id="news" />}
         />
         <PageHeader
+          locale={locale}
           menuItems={[
             {
               path: `/${locale}/news`,
