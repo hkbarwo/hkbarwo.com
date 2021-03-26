8020d903b0c03bd7adb4f1ddf748e8fb6e618d5d
Ng Wing Tat, David <i@ngwingt.at>
2021-03-27 05:09:26 +0800

    🐛 Fix Chinese stroke count in member directory

--- src/utils/gatsby/pages/member/directory.js
+++ src/utils/gatsby/pages/member/directory.js
@@ -1,6 +1,8 @@
-const cnchar = require('cnchar');
-const trad = require('cnchar-trad');
-cnchar.use(trad);
+const { 取笔顺 } = require('chinese-character-strokes');
+
+function getStrokeCount(char) {
+  return (取笔顺(char) || '').length
+}
 
 exports.createMemberDirectoryPage = async ({ actions, graphql }, context) => {
   const { locale, defaultLocale, pages: { 'member-directory': pageItem } } = context;
@@ -38,14 +40,14 @@ exports.createMemberDirectoryPage = async ({ actions, graphql }, context) => {
   const memberList = result.data.members.nodes.map(({ fields: { [locale]: member }}) => {
     const data = { ...member };
     if (locale === 'zh' && member.title) {
-      data.stroke = cnchar.stroke(member.title[0]);
+      data.stroke = getStrokeCount(member.title[0]);
     }
     if (data.stroke) {
     }
     return data;
   });
   if (locale === 'zh') {
-    memberList.sort((m1, m2) => m1.stroke - m2.stroke);
+    memberList.sort((m1, m2) => m1.title.localeCompare(m2.title, "zh-Hant"));
   }
   memberList.forEach((member) => {
     let directoryIndex;
