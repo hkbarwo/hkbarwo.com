2447ec1896b1a5f509f3c580acbeb37c2dcdf72c
Ng Wing Tat, David <i@ngwingt.at>
2021-09-23 15:50:33 +0800

    🥅 [Past Performances] Fix empty image

--- src/templates/PerformancesPastPage.js
+++ src/templates/PerformancesPastPage.js
@@ -32,7 +32,9 @@ export default function PerformancesPastPage(props) {
           {events.map((event, i) => (
             <li key={event.slug} className={classNames('md:flex', { 'mt-96': i > 0 })}>
               <div className="flex-shrink-0 w-full max-w-md">
-                <img src={event.coverImage} alt={event.title} />
+                {event.coverImage && (
+                  <img src={event.coverImage} alt={event.title} />
+                )}
               </div>
               <div className="flex-grow md:ml-120">
                 <h1 className="mt-20 font-black text-24">{event.title}</h1>
